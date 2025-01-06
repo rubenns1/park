5758cd6e76909d03a68d02cbb9cd0d09e9ecfd7b
Rubens Gomes <gomez.rubens@live.com>
2025-01-06 17:39:26 -0300

    Aplicação Spring Boot fase 2 FIAP

--- src/main/java/com/park/park/controller/VeiculosController.java
+++ src/main/java/com/park/park/controller/VeiculosController.java
@@ -7,12 +7,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.web.IWebExchange;
+
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 @Controller
 public class VeiculosController {
 
     @Autowired
     private VeiculosRepository veiculosRepository;
+
+    @RequestMapping("/vagas")
+    public String veiculosController(){
+        return "vagas";
+    }
 
     @RequestMapping("/")
     public ModelAndView modelAndView() {
@@ -25,6 +38,6 @@ public class VeiculosController {
     @RequestMapping(value = "/vagas", method = RequestMethod.POST)
     public String veiculosController(Veiculos veiculos) {
         veiculosRepository.save(veiculos);
-        return "redirect:/";
+        return "redirect:/vagas";
     }
 }
--- src/main/java/com/park/park/model/Veiculos.java
+++ src/main/java/com/park/park/model/Veiculos.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 @Entity
 public class Veiculos implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1l;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
